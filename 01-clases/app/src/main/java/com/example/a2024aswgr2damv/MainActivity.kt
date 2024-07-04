a9bf2965ba9bc927964c42bf5bff9a6cbe4ee662
Daniel <daniel.mera@epn.edu.ec>
2024-07-04 08:54:59 -0500

    Add: Intents implicito y explicito

--- 01-clases/app/src/main/java/com/example/a2024aswgr2damv/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2damv/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.a2024aswgr2damv
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +29,34 @@ class MainActivity : AppCompatActivity() {
         botonIrListView.setOnClickListener{
             irActividad(BListView::class.java)
         }
+
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito.setOnClickListener{
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackContenidoIntentImplicito.launch(intentConRespuesta)
+        }
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito.setOnClickListener{
+            val intentExplicito = Intent(
+                this,
+                CIntentExplicitoParametros::class.java
+            )
+            intentExplicito.putExtra("nombre", "Daniel")
+            intentExplicito.putExtra("apellido", "Mera")
+            intentExplicito.putExtra("edad", 22)
+            intentExplicito.putExtra(
+                "entrenador",
+                BEntrenador(10,"Daniel","mera")
+            )
+            callbackContenidoIntentExplicito.launch(intentExplicito)
+        }
     }
 
     fun irActividad(
@@ -31,5 +65,56 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this,clase)
         startActivity(intent)
     }
+
+    fun mostrarSnackbar(texto:String){
+        val snack = Snackbar.make(
+            findViewById(R.id.id_layout_main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == RESULT_OK){
+                if(result.data != null){
+                    // logica del negocio
+                    if(result.data!!.data != null){
+
+                    }
+                }
+            }
+        }
+
+    val callbackContenidoIntentImplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data!!.data != null){
+                    // logica del negocio
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackbar("Telefono $telefono")
+                    }
+                }
+            }
+        }
+
+
 
 }
