301f1de7c61afb1068887b831f5f082e0283b895
Daniel <daniel.mera@epn.edu.ec>
2024-07-12 10:59:23 -0500

    implementacion sqlite

--- 01-clases/app/src/main/java/com/example/a2024aswgr2damv/ESqliteHelperEntrenador.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2damv/ESqliteHelperEntrenador.kt
@@ -5,16 +5,16 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class ESqliteHelperEntrenador (
+class ESqliteHelperEntrenador(
     contexto: Context? // this
-):SQLiteOpenHelper(
+): SQLiteOpenHelper(
     contexto,
     "moviles",
     null,
     1
-){
+) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val scriptSQLiteCrearTablaEntrenador =
+        val scriptSQLCrearTablaEntrenador =
             """
                 CREATE TABLE ENTRENADOR(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,43 +22,41 @@ class ESqliteHelperEntrenador (
                     descripcion VARCHAR(50)
                 )
             """.trimIndent()
-        db?.execSQL(scriptSQLiteCrearTablaEntrenador)
+        db?.execSQL(scriptSQLCrearTablaEntrenador)
     }
-
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-
-    }
-
+    override fun onUpgrade(
+        p0: SQLiteDatabase?, p1: Int, p2: Int) {}
     fun crearEntrenador(
         nombre: String,
-        descripcion: String
-    ):Boolean{
+        descripcion:String
+    ):Boolean {
         val basedatosEscritura = writableDatabase
         val valoresAGuardar = ContentValues()
         valoresAGuardar.put("nombre", nombre)
         valoresAGuardar.put("descripcion", descripcion)
-        val resultadoGuardar = basedatosEscritura.insert(
-            "ENTRENADOR", // nombre tabla
-            null,
-            valoresAGuardar // valores
-        )
+        val resultadoGuardar = basedatosEscritura
+            .insert(
+                "ENTRENADOR", // nombre tabla
+                null,
+                valoresAGuardar // valores
+            )
         basedatosEscritura.close()
-        return if(resultadoGuardar.toInt()==-1) false else true
+        return if(resultadoGuardar.toInt() == -1) false else true
     }
 
-    fun eliminarEntrenadorFormulario (id:Int):Boolean{
+    fun eliminarEntrenadorFormulario(id:Int):Boolean{
         val conexionEscritura = writableDatabase
-        //Consulta SQL: where ... ID=? AND NOMBRE=? [?=1,?=2]
+        // Consulta SQL: where ... ID=? AND NOMBRE=?  [?=1, ?=2]
         val parametrosConsultaDelete = arrayOf(id.toString())
-        val resultadoEliminacion = conexionEscritura.delete(
-            "Entrenador",
-            "id=?",
-            parametrosConsultaDelete
-        )
+        val resultadoEliminacion = conexionEscritura
+            .delete(
+                "ENTRENADOR",
+                "id=?",
+                parametrosConsultaDelete
+            )
         conexionEscritura.close()
-        return if (resultadoEliminacion.toInt()==-1) false else true
+        return if(resultadoEliminacion.toInt()==-1) false else true
     }
-
     fun actualizarEntrenadorFormulario(
         nombre:String, descripcion: String, id: Int
     ):Boolean{
@@ -66,38 +64,42 @@ class ESqliteHelperEntrenador (
         val valoresAActualizar = ContentValues()
         valoresAActualizar.put("nombre", nombre)
         valoresAActualizar.put("descripcion", descripcion)
-        //where: ....
-        val parametrosConsultaActualizar = arrayOf(id.toString())
-        val resultadoActualizacion = conexionEscritura.update(
-            "Entrenador",
-            valoresAActualizar,
-            "id=?",
-            parametrosConsultaActualizar
-        )
+        // where: .....
+        val parametrosConsultaActualizar = arrayOf( id.toString())
+        val resultadoActualizacion = conexionEscritura
+            .update(
+                "ENTRENADOR",
+                valoresAActualizar, // nombre= Adrian, descripcion=B
+                "id=?", // id=1
+                parametrosConsultaActualizar // [1]
+            )
         conexionEscritura.close()
-        return if (resultadoActualizacion.toInt()==-1) false else true
+        return if(resultadoActualizacion.toInt()==-1) false else true
     }
 
-    fun consultarEntrenadorPorID(id: Int):BEntrenador?{
+    fun consultarEntrenadorPorID(id:Int):BEntrenador?{
         val baseDatosLectura = readableDatabase
         val scriptConsultaLectura = """
-            SELECT + FROM ENTRENADOR WHERE ID = ?
+            SELECT * FROM ENTRENADOR WHERE ID = ?
         """.trimIndent()
         val arregloParametrosConsultaLectura = arrayOf(
             id.toString()
         )
-        val resultadoConsultaLectura = baseDatosLectura.rawQuery(
-            scriptConsultaLectura,
-            arregloParametrosConsultaLectura
-        )
-        // Logica de busqueda
+        val resultadoConsultaLectura = baseDatosLectura
+            .rawQuery(
+                scriptConsultaLectura,
+                arregloParametrosConsultaLectura
+            )
+
+        // Logica busqueda
         // Recibimos un arreglo (puede ser nulo)
         // Llenar un arreglo de Entrenadores
         // Si esta vacio, el arreglo no tiene elementos
-        val existeAlMenosUno = resultadoConsultaLectura.moveToFirst()
+        val existeAlMenosUno = resultadoConsultaLectura
+            .moveToFirst()
         val arregloRespuesta = arrayListOf<BEntrenador>()
         if(existeAlMenosUno){
-            do {
+            do{
                 val entrenador = BEntrenador(
                     resultadoConsultaLectura.getInt(0),
                     resultadoConsultaLectura.getString(1),
@@ -108,7 +110,7 @@ class ESqliteHelperEntrenador (
         }
         resultadoConsultaLectura.close()
         baseDatosLectura.close()
-        return arregloRespuesta[0]
+        // ESQLiteHelperEntrenador.consultarEntrenadorPOrID
+        return if(arregloRespuesta.size>0) arregloRespuesta[0] else null
     }
-
 }
--- 01-clases/app/src/main/java/com/example/a2024aswgr2damv/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2damv/MainActivity.kt
@@ -12,59 +12,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val botonCicloVida = findViewById<Button>(
-            R.id.btn_ciclo_vida
-        )
-        botonCicloVida.setOnClickListener{
-            irActividad(ACicloVida::class.java)
-        }
-
-        val botonIrListView = findViewById<Button>(
-            R.id.btn_ir_list_view
-        )
-        botonIrListView.setOnClickListener{
-            irActividad(BListView::class.java)
-        }
-
-        val botonIntentImplicito = findViewById<Button>(
-            R.id.btn_ir_intent_implicito
-        )
-        botonIntentImplicito.setOnClickListener{
-            val intentConRespuesta = Intent(
-                Intent.ACTION_PICK,
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-            )
-            callbackContenidoIntentImplicito.launch(intentConRespuesta)
-        }
-        val botonIntentExplicito = findViewById<Button>(
-            R.id.btn_ir_intent_explicito
-        )
-        botonIntentExplicito.setOnClickListener{
-            val intentExplicito = Intent(
-                this,
-                CIntentExplicitoParametros::class.java
-            )
-            intentExplicito.putExtra("nombre", "Daniel")
-            intentExplicito.putExtra("apellido", "Mera")
-            intentExplicito.putExtra("edad", 22)
-            intentExplicito.putExtra(
-                "entrenador",
-                BEntrenador(10,"Daniel","mera")
-            )
-            callbackContenidoIntentExplicito.launch(intentExplicito)
-        }
-    }
-
-    fun irActividad(
-        clase: Class<*>
-    ){
-        val intent = Intent(this,clase)
-        startActivity(intent)
-    }
 
     fun mostrarSnackbar(texto:String){
         val snack = Snackbar.make(
@@ -79,13 +26,14 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ){
-            result ->
-            if(result.resultCode == RESULT_OK){
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
                 if(result.data != null){
-                    // logica del negocio
-                    if(result.data!!.data != null){
-
-                    }
+                    // logica negocio
+                    val data = result.data;
+                    mostrarSnackbar(
+                        "${data?.getStringExtra("nombreMofificado")}"
+                    )
                 }
             }
         }
@@ -94,10 +42,10 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ){
-            result ->
+                result ->
             if(result.resultCode == Activity.RESULT_OK){
-                if(result.data!!.data != null){
-                    // logica del negocio
+                if(result.data != null){
+                    // logica negocio
                     if(result.data!!.data != null){
                         val uri: Uri = result.data!!.data!!
                         val cursor = contentResolver.query(
@@ -115,6 +63,72 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val botonCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+        // Main Activity
+        val botonIrListView = findViewById<Button>(
+            R.id.btn_ir_list_view
+        )
+        botonIrListView
+            .setOnClickListener {
+                irActividad(BListView::class.java)
+            }
 
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackContenidoIntentImplicito.launch(intentConRespuesta)
+            }
+
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito
+            .setOnClickListener{
+                val intentExplicito = Intent(
+                    this,
+                    CIntentExplicitoParametros::class.java
+                )
+                intentExplicito.putExtra("nombre", "George")
+                intentExplicito.putExtra("apellido", "Quishpe")
+                intentExplicito.putExtra("edad", 22)
+                intentExplicito.putExtra(
+                    "entrenador", BEntrenador( 10, "George", "Quishpe" )
+                )
+                callbackContenidoIntentExplicito.launch(intentExplicito)
+            }
+
+        // Inicializar Base de Datos
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(
+            this
+        )
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite.setOnClickListener{
+            irActividad(ECrudEntrenador::class.java)
+        }
+
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this,clase)
+        startActivity(intent)
+    }
 }
